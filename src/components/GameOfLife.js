a78f48f1f1a0456b631573d48aa25e4fa46f66ab
Santiago N.S <gigogisantisd@gmail.com>
2024-07-05 20:50:06 -0300

    Logic of the game

--- src/components/GameOfLife.js
+++ src/components/GameOfLife.js
@@ -9,12 +9,81 @@ function GameOfLife() {
     for (let row = 0; row < numberRows; row++) {
       const newRow = [];
       for (let col = 0; col < numberCol; col++) {
-        newRow.push({ id: `${row}-${col}`, alive: false });
+        newRow.push({ id: `${row}-${col}`, alive: false});
       }
       newWorld.push(newRow);
     }
     setWorld(newWorld);
   };
+
+  const [gameRunning,setGameRunning] = useState(false)
+
+  /* game running */
+  useEffect(() => {
+    let intervalId;
+
+    if (gameRunning) {
+      intervalId = setInterval(() => {
+        nextTurn()
+      }, 1000);
+    }
+
+    // Limpiar el intervalo cuando `gameRunning` cambie o el componente se desmonte
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [gameRunning]);
+
+  const nextTurn = () => {
+    /*
+    1.	Si un cuadrado de la grilla está rodeado por tres células vecinas entonces se crea vida, agregándose una célula en ese sitio.
+    2.	Si una célula tiene menos de 2 células vecinas, muere por aislamiento y la casilla de la grilla correspondiente queda vacía, y si tiene más de 3 células vecinas, muere por sofocación y también se libera el cuadrado de la grilla que le correspondía.
+    3.	Una célula que tiene exactamente dos o tres vecinas sobrevive una instancia más en el juego.
+    */
+    const newWorld = [...world]
+    
+    world.map((row,index1) => {
+      row.map((col,index2) => {
+        if(world[index1][index2].alive == false){
+          const numberLivingNeighborCells = numberOfLivingNeighborCells(index1,index2)
+
+          if(numberLivingNeighborCells == 3)
+            newWorld[index1][index2].alive = true
+        }
+        else{
+          const numberLivingNeighborCells = numberOfLivingNeighborCells(index1,index2)
+          console.log(numberLivingNeighborCells)
+          if(numberLivingNeighborCells < 2 || numberLivingNeighborCells > 3)
+            newWorld[index1][index2].alive = false
+          else
+            newWorld[index1][index2].alive = true
+        }
+      })
+    })
+    
+    setWorld(newWorld)
+  }
+
+  const numberOfLivingNeighborCells = (x,y) => {
+    let numberLivingNeighborCells = 0
+
+    for(let row=x-1; row<=x+1; row++){
+      for(let col=y-1; col<=y+1; col++){
+        if(row === x && col === y) continue;
+
+        if(validCoordinates(row,col) && world[row][col].alive == true)
+          numberLivingNeighborCells++
+      }
+    }
+    return numberLivingNeighborCells
+  }
+
+  const validCoordinates = (x,y) => {
+    if(x<0 || x>49 || y<0 || y>49)
+      return false
+    else
+      return true
+  }
 
   /* Click handlers */
   const [isMouseDown, setIsMouseDown] = useState(false);
@@ -55,6 +124,8 @@ function GameOfLife() {
 
   return (
     <div className="app">
+      <button onClick={() => setGameRunning(true)}>start</button>
+      <button onClick={() => setGameRunning(false)}>finish</button>
       <div id="board-container">
         <table onMouseLeave={() => setIsMouseDown(false)}>
           <tbody>
