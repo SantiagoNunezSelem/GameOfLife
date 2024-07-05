47a719bec8fedace390ec10ec42318e18d869110
Santiago N.S <gigogisantisd@gmail.com>
2024-07-05 01:20:17 -0300

    Board and click handling method created

--- src/components/GameOfLife.js
+++ src/components/GameOfLife.js
@@ -1,40 +1,81 @@
-import React,{useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "../stylesheets/GameOfLife.css";
 
-function GameOfLife(){
+function GameOfLife() {
+  const [world, setWorld] = useState([]);
 
-  const createWorld = (numberRows,numberCol) => {
-    let html = "<table>"
-    for(let rows=0;rows<numberRows;rows++){
-      html += "<tr>"
-      for(let col=0;col<numberCol;col++){
-        html += "<td id=cell>"
-        html += "</td>"
-        /*addCell(uuidv4(),i)
-        console.log()*/
+  const createWorld = (numberRows, numberCol) => {
+    const newWorld = [];
+    for (let row = 0; row < numberRows; row++) {
+      const newRow = [];
+      for (let col = 0; col < numberCol; col++) {
+        newRow.push({ id: `${row}-${col}`, alive: false });
       }
-      html += "</tr>"
+      newWorld.push(newRow);
     }
-    html += "</table>"
-    let container = document.getElementById("board-container")
-    container.innerHTML = html
+    setWorld(newWorld);
+  };
+
+  const [isMouseDown, setIsMouseDown] = useState(false);
+
+  const handleCellHold = (rowIndex, colIndex, ev) => {
+    
+    if(isMouseDown){
+      setWorld((prevWorld) => {
+        const newWorld = [...prevWorld];
+        newWorld[rowIndex][colIndex].alive = true;
+
+        return newWorld;
+      });
+    }
+  };
+
+  const handleMouseDown = () => {
+    setIsMouseDown(true);
+  };
+
+  const handleMouseUp = () => {
+    setIsMouseDown(false);
+  };
+
+  const handleCellClick = (rowIndex, colIndex) => {
+    const newWorld = [...world]
+
+    newWorld[rowIndex][colIndex].alive = !newWorld[rowIndex][colIndex].alive
+
+    setWorld(newWorld)
   }
 
   useEffect(() => {
-    const numberRows = 20;
-    const numberCol = 20;
-    createWorld(numberRows,numberCol);
-  },[])
+    const numberRows = 30;
+    const numberCol = 30;
+    createWorld(numberRows, numberCol);
+  }, []);
 
-
-
-  return(
+  return (
     <div className="app">
-
       <div id="board-container">
-
+        <table onMouseLeave={() => setIsMouseDown(false)}>
+          <tbody>
+            {world.map((row, rowIndex) => (
+              <tr key={rowIndex}>
+                {row.map((cell, colIndex) => (
+                  <td
+                    key={cell.id}
+                    id={`cell-${rowIndex}-${colIndex}`}
+                    className={cell.alive ? "alive" : "dead"}
+                    onClick={() => handleCellClick(rowIndex, colIndex)}
+                    onMouseDown={() => handleMouseDown()}
+                    onMouseUp={() => handleMouseUp()}
+                    onMouseEnter={(ev) => handleCellHold(rowIndex, colIndex,ev)}
+                  >
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
-
     </div>
   );
 }
